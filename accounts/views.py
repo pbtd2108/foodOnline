ce67d7a30145222f03044c1ae46c76e2f8e841a7
Prabhati <pbtd2108@gmail.com>
2024-02-18 20:08:19 +0530

    registerUser and views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,22 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from accounts.forms import UserForm
 
 # Create your views here.
 
 def registeruser(request):
-    form=UserForm()
+    if request.method=='POST':
+        print(request.POST,"#############################################3")
+        form=UserForm(request.POST)
+        print("1111111111111")
+        if form.is_valid():
+            print("2222222222222")
+            form.save()
+            print("33333333333333")
+            return redirect('registeruser')
+    else:
+        form=UserForm()  
     context={
         'form':form,
     }
